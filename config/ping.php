6553c73d0adf937f3564f2c470469045cb64635c
Gabriel Otemu <otemugabriel@gmail.com>
2026-04-28 15:18:17 +0100

    Fixed DB connection and added ping endpoint

--- config/ping.php
+++ config/ping.php
@@ -1,11 +1,10 @@
 <?php
-// ping.php - lightweight keep-alive endpoint
-
 require __DIR__ . '/db.php';
 
-// very lightweight query to wake DB
-mysqli_query($conn, "SELECT 1");
+// use $link (NOT $conn)
+if (isset($link) && $link) {
+    mysqli_query($link, "SELECT 1");
+}
 
-// respond fast
 http_response_code(200);
 echo "OK";
